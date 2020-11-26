683b876dd3b39a08d4f63969bf3530eda6131794
peter <9004027+plsakr@users.noreply.github.com>
2020-11-26 13:10:04 +0200

    started with library

--- frontend/src/app/library/library.component.ts
+++ frontend/src/app/library/library.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./library.component.css']
 })
 export class LibraryComponent implements OnInit {
-
+  value = '';
   constructor() { }
 
   ngOnInit(): void {
